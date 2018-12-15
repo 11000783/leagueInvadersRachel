99ad76ec3b754791a76c32e9714cc60af13c58db
11000783 <josephaguilar2022@gmail.com>
2018-12-14 18:30:31 -0800

    Initial commit

---
+++ src/Game/LeagueInvadersRachel.java
@@ -0,0 +1,37 @@
+package Game;
+
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class LeagueInvadersRachel {
+	public static void main(String[] args) {
+		new LeagueInvadersRachel().setup();
+
+	}
+
+	JFrame frame = new JFrame();
+	GamePanel panel = new GamePanel();
+	
+
+	LeagueInvadersRachel() {
+		frame = new JFrame();
+		panel = new GamePanel();
+	}
+
+	void setup() {
+		panel = new GamePanel();
+		frame.add(panel);
+		frame.addKeyListener(panel);
+		final int x = 500;
+		final int y = 800;
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(x, y);
+		frame.getContentPane().setPreferredSize(new Dimension(x, y));
+        frame.pack();
+        new GamePanel().startGame();
+
+	}
+}
